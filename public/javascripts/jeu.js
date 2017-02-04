414bb239d5c97a44a66bf9b364ad508c8d4ea86d
Vincent Emond <criv02@uqo.ca>
2017-02-04 17:53:29 -0500

    Codé une preuve de concept initiale. Les cartes sont distribuées.

---
+++ public/javascripts/jeu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("#NewGame").click(function() {
+		$.post("/start", afficherMains );
+	});
+});
+
+function afficherMains(data) {
+	var jeu = data;
+	
+	$("#tableDeJeu").empty();
+	$("#tableDeJeu").html('<h2>Joueur 1</h2><p id="j1"></p><h2>Joueur 2</h2><p id="j2"></p><h2>Joueur 3</h2><p id="j3"></p><h2>Joueur 4</h2><p id="j4"></p>');
+	
+	data.joueurs.forEach(function(item,indexJoueur) {
+		item.main.forEach(function(item,indexCarte) {
+			$('#j' + (indexJoueur+1)).append('<img src="images/cartes/' + item.numero + '_' + item.genre + '.png" width="50" height="73"  alt="carte"></img>');
+		});
+	});
+}
