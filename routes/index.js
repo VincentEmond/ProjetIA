13ffa8eb23d5cb1b47acb71a0bfc6e8c3e0f905d
Vince <Vince>
2017-03-20 22:12:51 -0400

    Ajouté des commentaires. C'est le joueur qui ramasse qui joue le prochain.

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/view', function(req, res, next) {
 	
 });
 
+//Retourne le numero de joueur (index) au client pour qu'il sache quel joueur il est.
 router.post('/start', function(req, res, next) {
 	
 	var db = res.app.get('firebase').database();
@@ -21,6 +22,7 @@ router.post('/start', function(req, res, next) {
 	
 	var jeu = getEtat(req);
 	
+	//Si le jeu n'existe pas il faut l'initialiser.
 	if (jeu == undefined)
 	{
 		jeu = {};
@@ -37,6 +39,7 @@ router.post('/start', function(req, res, next) {
 	}
 	else
 	{
+		//Le jeu est une machine a état finis. On peut jouer lorsqu'il entre dans l'état READY.
 		switch (jeu.etat)
 		{
 			case "WAITING_FOR_4":
@@ -63,8 +66,9 @@ router.post('/start', function(req, res, next) {
 					//on active tout et on ilimine
 					activeToutes(jeu.joueurs, true);
 					desactiveAutresJoueurs(jeu.joueurs, jeu.tour);
-					//Pas le droit de jouer la dame de pique au premier tour
+					//Pas le droit de jouer la dame de pique au premier tour.
 					desactiveCarte(jeu.joueurs, { numero: 12, genre: "Pique"});
+					//Pas le droit de jouer du coeur au premier tour.
 					desactiveCoeur(jeu.joueurs[jeu.tour].main, true);
 				}
 				console.log("WAITING_FOR_1");
@@ -141,6 +145,7 @@ router.post("/leave", function(req, res, next) {
 	res.end("Success");
 });
 
+//Appelé par le client. Il nous envoie une carte et son numero de joueur.
 router.post("/jouer", function(req, res, next) {
 	
 	var db = res.app.get('firebase').database();
@@ -150,12 +155,14 @@ router.post("/jouer", function(req, res, next) {
 	
 	var jeu = getEtat(req);
 	
+	//Si le jeu n'est pas dans l'etat READY alors on ne devrait pas être ici.
 	if (jeu == undefined || jeu.etat != "READY") {
 		res.status(400);
 		res.end("Pas le droit de jouer tant que le jeu n'a pas commencé");
 		return;
 	}
 	
+	//si le numero de joueur n'est pas égal au tour dans l'état du jeu alors c'est pas le tour de ce joueur de jouer.
 	if (move.joueur != jeu.tour) {
 		res.status(400);
 		res.end("Ce n'est pas votre tour.");
@@ -164,16 +171,19 @@ router.post("/jouer", function(req, res, next) {
 	
 	var index;
 	
+	//Prendre la main du joueur qui joue.
 	main = jeu.joueurs[move.joueur].main;
-	//Enlever la carte de la main
+
+	//Enlever la carte jouée de la main
 	index = trouveCarte(main, move.carte);
 	main.splice(index, 1);
 	
 	//Mettre la carte en jeu
 	jeu.joueurs[move.joueur].enJeu = move.carte;
 	
-	//Avancer le tour de 1
-	jeu.tour = (jeu.tour+1)%4
+	//Avancer le tour de 1 si on est pas le dernier tour de la round.
+	if (jeu.compteurTour != 3)
+		jeu.tour = (jeu.tour+1)%4
 	
 	//Si le joueur joue du coeur il a brisee le coeur
 	if (move.carte.genre == "Coeur")
@@ -182,19 +192,20 @@ router.post("/jouer", function(req, res, next) {
 	//On active toutes les cartes et on y va par elimination
 	activeToutes(jeu.joueurs, true);
 	
-	//Si le coeur n'a pas ete brise et que le joueur a autre chose que du coeur.
-	if (!jeu.coeurBrise && !justeDuCoeur(jeu.joueurs[jeu.tour].main))
-		desactiveCoeur(jeu.joueurs[jeu.tour].main, true);
+	
 	
 	//C'est le debut d'une round
 	if (jeu.compteurTour == 0 ) {
+		//On initialise le type de carte demandé au type que le premier joueur de la round a joué.
 		jeu.genreDemande = move.carte.genre;
+		//Sinon si c'est le dernier tour d'une round.
 	} else if (jeu.compteurTour == 3) {
 		//On laisse le temps de montrer a tous le monde le dernier move
 		jeu.etat = "DELAY";
 		ref.set(jeu);
 		setEtat(req, jeu);
 		
+		//Cette fonction s'active après un délai de 5 secondes.
 		setTimeout(function(data) {
 			var resultat = evaluerRamasse(data.jeu);
 			console.log(resultat);
@@ -207,12 +218,29 @@ router.post("/jouer", function(req, res, next) {
 			}
 			
 			ramasseux.pts += resultat.pts;
+
+			//Le tour est a celui qui ramasse
+			console.log("Le joueur qui ramasse est: " + (resultat.joueur + 1));
+			jeu.tour = resultat.joueur;
+
+			//Si le coeur n'a pas ete brise et que le prochain joueur a autre chose que du coeur.
+			//alors on dois desactivé le coeur.
+			if (!jeu.coeurBrise && !justeDuCoeur(jeu.joueurs[jeu.tour].main))
+				desactiveCoeur(jeu.joueurs[jeu.tour].main, true);
+
 			data.jeu.etat = "READY";
 			doRest(data);
-		}, 10*1000, {request: req, response: res, jeu: jeu, ref: ref} );
+		}, 5*1000, {request: req, response: res, jeu: jeu, ref: ref} );
 		return;
 	}
+
+	//On est au début ou au milieu de la round.
 	
+	//Si le coeur n'a pas ete brise et que le prochain joueur a autre chose que du coeur.
+	//alors on dois desactivé le coeur.
+	if (!jeu.coeurBrise && !justeDuCoeur(jeu.joueurs[jeu.tour].main))
+		desactiveCoeur(jeu.joueurs[jeu.tour].main, true);
+
 	//Avance le compteur de tour
 	jeu.compteurTour = (jeu.compteurTour+1)%4;
 	
@@ -264,13 +292,14 @@ function doRest(data) {
 	res.end("Success");
 }
 
+//Retourne un tableau de 4 joueurs initialisé avec leur 13 cartes.
 function initJoueurs() {
 	var joueurs = [];
 	var paquet = createPaquet();
 	var current;
 	var i = 0;
 
-	
+	//Initialise les propriétés de chaque objet joueur dans le tableau de joueur.
 	for (var j=0; j<4; j++) {
 		joueurs[j] = {}
 		joueurs[j].main = [];
@@ -280,7 +309,7 @@ function initJoueurs() {
 		joueurs[j].pile = [];
 	}
 	
-	
+	//Distribue les cartes en alternant entre chaque joueur.
 	while ((current = paquet.pop()) != undefined) {
 		joueurs[i].main.push(current);
 		i= ++i%4
@@ -289,16 +318,19 @@ function initJoueurs() {
 	return joueurs;
 }
 
+//Retourne l'etat du jeu sauvegardé dans la cache
 function getEtat(req) {
 	var myCache = req.app.get("myCache");
 	return myCache.get("jeu");
 }
 
+//Sauvegarde l'état du jeu dans la cache
 function setEtat(req, jeu) {
 	var myCache = req.app.get("myCache");
 	myCache.set("jeu", jeu);
 }
 
+//Retourne l'index d'une carte dans la main d'un joueur.
 function trouveCarte(arr, carte) {
 	for (var i=0; i<arr.length; i++) {
 		if (arr[i].numero == carte.numero && arr[i].genre == carte.genre) {
@@ -308,6 +340,7 @@ function trouveCarte(arr, carte) {
 	}
 }
 
+//Retourne vrai si une main contient juste du coeur sinon faux.
 function justeDuCoeur(main) {
 	for (var i=0; i<main.length; i++) {
 		if (main[i].genre != "Coeur")
@@ -316,6 +349,7 @@ function justeDuCoeur(main) {
 	return true;
 }
 
+//Si desactive est vrai alors desactive tout le coeur d'une main sinon l'active.
 function desactiveCoeur(main, desactive) {
 	for (var i=0; i<main.length; i++) {
 		if (main[i].genre == "Coeur")
@@ -323,6 +357,7 @@ function desactiveCoeur(main, desactive) {
 	}
 }
 
+//Retourne l'index du joueur ayant le deux de trefle
 function trouveDeuxTrefle(joueurs) {
 	for (var i=0; i<4; i++) {
 		for (var j=0; j<joueurs[i].main.length; j++) {
@@ -332,6 +367,7 @@ function trouveDeuxTrefle(joueurs) {
 	}
 }
 
+//Desactive la carte c passée en paramètre.
 function desactiveCarte(joueurs, c) {
 	for (var i=0; i<4; i++) {
 		joueurs[i].main.forEach(function(carte) {
@@ -341,6 +377,7 @@ function desactiveCarte(joueurs, c) {
 	}
 }
 
+//Desactive toutes les cartes des jouers différent de tour
 function desactiveAutresJoueurs(joueurs, tour) {
 	for (var i=0; i<4; i++) {
 		if (i == tour) continue;
@@ -350,6 +387,7 @@ function desactiveAutresJoueurs(joueurs, tour) {
 	}
 }
 
+//Desactive toutes les cartes d'une main autre que le genre passé en paramètre.
 function desactiveAutresGenres(main, genre) {
 	for (var i=0; i<main.length; i++) {
 		if (main[i].genre != genre)
@@ -357,6 +395,7 @@ function desactiveAutresGenres(main, genre) {
 	}
 }
 
+//Retourne vrai s'il reste le genre passez en paramètre dans la main passez en paramètre.
 function resteDu(main, genre) {
 	for (var i=0; i<main.length; i++) {
 		if (main[i].genre == genre)
@@ -365,6 +404,7 @@ function resteDu(main, genre) {
 	return false;
 }
 
+//Activer toutes les cartes de tous les joueurs.
 function activeToutes(joueurs, active) {
 	for (var i=0; i<4; i++) {
 		joueurs[i].main.forEach(function(carte) {
@@ -373,6 +413,7 @@ function activeToutes(joueurs, active) {
 	}
 }
 
+//Retourne le joueur qui ramasse et le nombre de points qu'il se prends.
 function evaluerRamasse(jeu) {
 	var max = 0;
 	var pts = 0;
@@ -385,7 +426,7 @@ function evaluerRamasse(jeu) {
 			pts++;
 		
 		if (laCarte.genre == "Pique" && laCarte.numero == 12)
-			pts+=12;
+			pts+=13;
 		
 		//Seulement les cartes du genre demande compte
 		if (laCarte.genre == jeu.genreDemande) {
@@ -393,15 +434,17 @@ function evaluerRamasse(jeu) {
 				max = 9000; //L'as bat tout
 				joueur = i;
 			} else if (laCarte.numero > max) {
-				max = laCarte.numero;
+				max = parseInt(laCarte.numero);
 				joueur = i;
 			}
 		}
 	}
+
 	
 	return { joueur: joueur, pts: pts};
 }
 
+//Retourne un tableau de 52 cartes mélangé.
 function createPaquet() {
 	var genre = ['Trefle', 'Pique', 'Coeur', 'Carreau'];
 	var paquet = [];
@@ -424,6 +467,7 @@ function createPaquet() {
 	return shuffle(paquet);
 }
 
+//Mélange un tableau
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
